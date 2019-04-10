c4503a8bad1602b4c4c55ce197f07e0f4c0bcdbc
fkren <filip.kren@hycu.com>
2019-04-10 11:20:29 +0200

    fixed an error when parsing strings.

--- scan.py
+++ scan.py
@@ -152,10 +152,10 @@ def infected_s3_object(s3_object) :
                 }
             )
     except:
-        print ("Failed to move infected file : %s.%s %" (s3_object.bucket_name,s3_object.key))
+        print ("Failed to move infected file : %s.%s " % (s3_object.bucket_name,s3_object.key))
     else:
-        print("Infected file moved to :  %s.%s %" (infected_s3_object.bucket_name, infected_s3_object.key))
-        
+        print("Infected file moved to :  %s.%s " % (infected_s3_object.bucket_name, infected_s3_object.key))
+
 
 def lambda_handler(event, context):
     start_time = datetime.utcnow()
